dd05b3a6ef5ce58275792a472ffbd342d58c2767
RiseWeez <115504828+RiseWeez@users.noreply.github.com>
2022-11-20 22:55:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-// Get the modal
-const modal = document.getElementById('myModal');
+// modal
+const modal = document.getElementById('cartModal');
 
-// Get the button that opens the modal
 const btn = document.getElementById('buyButton');
 
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = 'block';
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = 'none';
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
   }
 };
 
+// collapsible slider
 const coll = document.getElementsByClassName('collapsible');
 let i;
 
@@ -38,3 +34,60 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// range slider
+
+let bonusValue = 500;
+let maxDiscount = 1498;
+let itemPrice = 1499;
+// let currencyValue = bonusValue;
+
+document.getElementById('bonusValue').innerHTML = bonusValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+// document.getElementById('currencyValue').innerHTML = currencyValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+document.getElementById('maxDiscount').innerHTML = maxDiscount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+document.getElementById('itemPrice').innerHTML = itemPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+function rangeSlide(value) {
+  const priceDiff = itemPrice - value;
+  // document.getElementById('selectedDiscount').innerHTML = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  // document.getElementById('discountBonusValue').innerHTML = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  document.getElementById('finalPrice').innerHTML = priceDiff.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  document.getElementById('withBonusPrice').innerHTML = priceDiff.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+  // document.getElementById('selectedDiscount').style.color = '#00a93e';
+  // document.getElementById('discountBonusValue').style.color = '#00a93e';
+
+  if (value > bonusValue) {
+    withBonusPrice.style.color = '#00a93e';
+  }
+}
+
+window.onload = function setSliderRange() {
+  let target = document.querySelectorAll('input[type="range"]')[0];
+  target.value = bonusValue;
+  let min = target.min;
+  target.max = itemPrice;
+
+  target.style.backgroundSize = ((bonusValue - min) * 100) / (itemPrice - min) + '% 100%';
+};
+
+// bonusValueInput.onchange = function changeBonusValue() {
+//   let bonusValueInput = document.getElementById('bonusValueInput').value;
+//   document.getElementById('bonusValue').innerHTML = bonusValueInput.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+//   // document.getElementById('currencyValue').innerHTML = bonusValueInput.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+//   // currencyValue = bonusValue;
+//   bonusValue = +bonusValueInput;
+
+//   let target = document.querySelectorAll('input[type="range"]')[0];
+//   target.value = bonusValueInput;
+//   let min = target.min;
+//   target.max = maxDiscount;
+
+//   target.style.backgroundSize = ((bonusValueInput - min) * 100) / (maxDiscount - min) + '% 100%';
+// };
+
+const input = document.querySelector('.range');
+
+input.addEventListener('change', () => {
+  if (input.value > bonusValue) input.value = bonusValue;
+});
